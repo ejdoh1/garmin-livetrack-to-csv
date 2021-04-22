8dfacba1e8e77a244fa87067391e486c179161f3
Erin Doherty <erin.doherty@team.telstra.com>
2021-04-22 13:41:38 +1000

    init

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# garmin-livetrack-to-csv
+# Garmin LiveTrack JSON to CSV API
  
+Converts a Garmin LiveTrack JSON feed to CSV for livestream video overlay integration.
+
+To use, get a Garmin activity session ID poll https://abc.execute-api.ap-southeast-2.amazonaws.com/dev/session/REPLACE_ME_SESSION_ID every 5s or so.
+
+## Deploy
+
+```sh
+# update/remove profile in serverless.yml
+# start docker
+npm i
+sls deploy
+```
---
+++ run.py
@@ -0,0 +1,19 @@
+import json
+import requests
+import time
+import pandas
+from pandas import json_normalize
+
+
+def run(event, context):
+    s = event['pathParameters']['session']
+    # t0 = time.time_ns() // 1_000_000
+    # t1 = int(t0 - 60 * 1e3)
+    # u = f"https://livetrack.garmin.com/services/session/{s}/trackpoints?requestTime={t0}&from={t1}"
+    u = f"https://livetrack.garmin.com/services/session/{s}/trackpoints"
+    r = requests.get(u).json()
+    n = json_normalize(r['trackPoints'])
+    return {
+        "statusCode": 200,
+        "body": n.to_csv()
+    }
